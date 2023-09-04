c2ae2043ab0075d5d3fd09041094a41cf1dfa5d7
korneliuszw <korneliuszwstudio@gmail.com>
2023-09-04 19:39:09 +0200

    Audi processing for recognition

---
+++ src/audio/AudioSource.ts
@@ -0,0 +1,35 @@
+import {KaldiRecognizer} from "vosk-browser";
+import {SAMPLE_RATE} from "../vosk/VoskManager";
+import registerAudioProcessor from 'audio-worklet:./processor/AudioProcessor.ts'
+
+export class AudioSource {
+    private _mediaStream: MediaStream
+    private _audioContext: AudioContext
+    private _recognizerNode: AudioWorkletNode
+    private async initAudio(recognizer: KaldiRecognizer, channel: MessageChannel) {
+        this._mediaStream = await navigator.mediaDevices.getUserMedia({audio: {
+                sampleRate: SAMPLE_RATE,
+                echoCancellation: true,
+                noiseSuppression: true,
+                channelCount: 1
+            }, video: false})
+        this._audioContext = new AudioContext()
+        // TODO: This won't work with bundlers
+        await registerAudioProcessor(this._audioContext)
+        this._recognizerNode = new AudioWorkletNode(this._audioContext, 'recognizer-processor', { channelCount: 1, numberOfInputs: 1, numberOfOutputs: 1 })
+        this._recognizerNode.port.postMessage({type: 'init', recognizerId: recognizer.id}, [channel.port2])
+    }
+
+    static async init(recognizer: KaldiRecognizer, channel: MessageChannel): Promise<AudioSource> {
+        const audioSource = new AudioSource()
+        await audioSource.initAudio(recognizer, channel)
+        return audioSource
+    }
+
+    public async stop() {
+        this._recognizerNode.disconnect()
+        await this._audioContext.close()
+        this._mediaStream.getTracks().forEach(track => track.stop())
+    }
+
+}
---
+++ src/audio/processor/AudioProcessor.ts
@@ -0,0 +1,53 @@
+// Based on https://github.com/ccoreilly/vosk-browser/blob/master/examples/modern-vanilla/recognizer-processor.js
+export interface AudioProcessorMessage {
+    type: 'init'
+    recognizerId?: number
+}
+
+export interface AudioProcessorOutgoingMessage {
+    action: 'audioChunk',
+    data: Float32Array,
+    recognizerId: number
+    sampleRate: number
+}
+
+class AudioProcessor extends AudioWorkletProcessor {
+
+    private _recognizerId?: number;
+    private _recognizerPort?: MessagePort;
+
+    constructor(options) {
+        super(options);
+        this.port.onmessage = this._processMessage.bind(this)
+    }
+
+    private _processMessage(message: MessageEvent<AudioProcessorMessage>) {
+        if (message.data.type === 'init') {
+            this._recognizerId = message.data.recognizerId;
+            this._recognizerPort = message.ports[0]
+        }
+    }
+
+    public process(
+        inputs: Float32Array[][],
+        outputs: Float32Array[][],
+        parameters: Record<string, Float32Array>
+    ): boolean {
+        const data = inputs[0][0]
+        if (!this._recognizerPort && data) return true
+        // AudioBuffer samples are represented as floating point numbers between -1.0 and 1.0 whilst
+        // Kaldi expects them to be between -32768 and 32767 (the range of a signed int16)
+        const audioArray = data.map(value => value * 0x800)
+        console.log(sampleRate)
+        const message: AudioProcessorOutgoingMessage = {
+            action: 'audioChunk',
+            data: audioArray,
+            recognizerId: this._recognizerId,
+            sampleRate // global scope of worklet
+        }
+        this._recognizerPort.postMessage(message, {transfer: [audioArray.buffer]})
+        return true;
+    }
+}
+
+registerProcessor('recognizer-processor', AudioProcessor)
